9c94b5528551d50022f96c130da89592b6b15f19
DENISE.DL <150688665+DENISEDL@users.noreply.github.com>
2023-11-30 11:01:00 +0100

    Add files via upload

---
+++ MyFirstClassStudente/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Studente studente1 = new Studente("Edoardo", "Bisio", 180004);
+        System.out.print(studente1);
+
+
+    }
+}

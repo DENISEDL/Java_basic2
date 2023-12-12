2c3b9bbbbf72db42522a23857ccf84c9e3721f69
DENISE.DL <denise.dl@hotmail.it>
2023-12-12 14:09:08 +0100

    Modifica esercizio 11

--- Esercizio11_/src/Main.java
+++ Esercizio11_/src/Main.java
@@ -4,14 +4,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        controlloIntinUnDetermiantoRange();
-
+        controlloIntinUnDetermiantoRange(1,2,2);
     }
 
-    public static void controlloIntinUnDetermiantoRange(){
-        int num1= 1;
-        int num2= 3;
-        int numVerifica = 3 ;
+    public static void controlloIntinUnDetermiantoRange(int num1,int num2,int numVerifica){
+
             if (numVerifica < num1 || numVerifica > num2 || num1 > num2) {
                 throw new ArithmeticException();
 

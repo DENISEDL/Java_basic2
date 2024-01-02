f50682fe3abaff2d7307ca651b67f03b6b137180
DENISE.DL <denise.dl@hotmail.it>
2024-01-02 11:01:44 +0100

    fatto

--- Esericizio24_/src/Main.java
+++ Esericizio24_/src/Main.java
@@ -1,7 +1,22 @@
-//Creare un hashmap, prendere tutti i valori, ordinarli e stamparligit
+//Creare un hashmap, prendere tutti i valori, ordinarli e stamparli
+
+
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<String,Integer> etaCuccioliCane = new HashMap<>();
+        etaCuccioliCane.put("Bobby",2);
+        etaCuccioliCane.put("Pepita",6);
+        etaCuccioliCane.put("Tokyo",5);
+        etaCuccioliCane.put("Kira",9);
+
+        Collection<Integer> value = etaCuccioliCane.values();
+        List<Integer> listaValueOrdinata = new ArrayList<>(value);
+        Collections.sort(listaValueOrdinata);
+        System.out.println("Oridine di anni dei cuccioli presenti nel canile : " + listaValueOrdinata);
+
     }
 }

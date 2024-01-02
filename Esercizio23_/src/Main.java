af93662f3fbe5d744af3d543f6e057386c37ad8f
DENISE.DL <denise.dl@hotmail.it>
2024-01-02 10:59:19 +0100

    fatto

--- Esercizio23_/src/Main.java
+++ Esercizio23_/src/Main.java
@@ -1,5 +1,36 @@
+//Creare 3 hashmap con 3 differenti metodi di inizializzazione e stampare il contenuto.
+
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Map<String,Integer> capStudenti = new HashMap<>();
+        capStudenti.put("CS",87100);
+        capStudenti.put("RC",89121);
+        capStudenti.put("NA",80013);
+        capStudenti.put("MI",20019);
+        System.out.println("Cap postale degli Studenti : " + capStudenti);
+
+
+        Map<String,Integer> studenti = Map.of("Denise",28,
+                "Marc",23,"Luigi",30,"Paolo",22);
+        System.out.println("Studenti : " + studenti);
+
+
+        Map<String,String> scuole =  Map.ofEntries(
+                new AbstractMap.SimpleEntry<>("Liceo","Scientifico"),
+                new AbstractMap.SimpleEntry<>("Professionale","Odontotecnico"),
+                new AbstractMap.SimpleEntry<>("Industriale","Meccanico")
+        );
+        System.out.println("Scuole : " + scuole);
+
+
+
+
+
     }
 }
---
+++ Esericizio24_/src/Main.java
@@ -0,0 +1,7 @@
+//Creare un hashmap, prendere tutti i valori, ordinarli e stamparligit
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

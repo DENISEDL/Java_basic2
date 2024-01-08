bed11854776374b665ed628af3ced7abd803a7c0
DENISE.DL <denise.dl@hotmail.it>
2024-01-08 09:38:55 +0100

    fattoÂ

---
+++ Esercizio29_/src/Main.java
@@ -0,0 +1,19 @@
+//Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+//aggiungi un anno
+//sottrai un mese
+//aggiungi 7 giorni
+//Stampa il risultato localizzata per l'Italia
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        OffsetDateTime dataAggiornata = data.plusYears(1).minusMonths(1).plusDays(7);
+        System.out.println("Data aggiornata: " + dataAggiornata);
+    }
+}

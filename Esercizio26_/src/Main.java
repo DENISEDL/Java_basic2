026d9add7c4c989b5108fdec3892abceac7b4730
DENISE.DL <denise.dl@hotmail.it>
2024-01-08 08:36:23 +0100

    fatto

---
+++ Esercizio26_/src/Main.java
@@ -0,0 +1,20 @@
+//Crea un oggetto OffsetDateTime da questa stringa 2002-03-01T13:00:00Z
+//Formatta la data ottenuta in FULL, MEDIUM e SHORT
+//Stampa le varie versioni
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+        String stampaLong = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        System.out.println("Stampa per intero : " + stampaLong);
+        String stampaMedium = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+        System.out.println("Stampa il mese abbreviato : " + stampaMedium);
+        String stampaShort = data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
+        System.out.println("Stampa abbreviato : " + stampaShort);
+
+    }
+}

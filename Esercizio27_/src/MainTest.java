0adcad0d9d99115730fb4b03d076ba9016d06a6e
DENISE.DL <denise.dl@hotmail.it>
2024-01-11 11:05:16 +0100

    Aggiunto test all'esercizio delle date

---
+++ Esercizio27_/src/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void testaDataSpecifica() {
+        assertEquals("2023-03-01T13:00:00Z","2023-03-01T13:00:00Z");
+
+    }
+}

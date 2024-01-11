d3202b5ea933c3f080c62589315c03fc0461050e
DENISE.DL <denise.dl@hotmail.it>
2024-01-11 11:28:51 +0100

    ÂSovrascritto esercizio con aggiunta di un test

--- Esercizio27_/src/MainTest.java
+++ Esercizio27_/src/MainTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -7,4 +9,10 @@ class MainTest {
         assertEquals("2023-03-01T13:00:00Z","2023-03-01T13:00:00Z");
 
     }
+    @org.junit.jupiter.api.Test
+    void testaFormatoDataLong() {
+        assertEquals("1 march 2002","1 march 2002");
+
+    }
+
 }

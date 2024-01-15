a4fd1ef997c6fe37d2e2a7c50fb0aa05cb4a14c4
DENISE.DL <denise.dl@hotmail.it>
2024-01-15 10:35:50 +0100

    fatto

---
+++ Esercizio33_OperatoreTernario2/src/Main.java
@@ -0,0 +1,36 @@
+//public class Module01 {
+//    public static void main(String[] args) {
+//        System.out.println(checkString("Questa è una stringa"));
+//    }
+//
+//    private static String checkString(String value) {
+//        String result;
+//        if (value.length() >= 10) {
+//            result = "Lunghezza maggiore o uguale di 10";
+//        } else {
+//            result = "Lunghezza minore di 10";
+//        }
+//        return result;
+//    }
+//}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        /*if (value.length() >= 10) {
+            result = "Lunghezza maggiore o uguale di 10";
+        } else {
+            result = "Lunghezza minore di 10";
+        }
+        return result;*/
+        final String result = value.length() >= 10
+                ? "Lunghezza maggiore o uguale di 10"
+                : "Lunghezza minore di 10";
+        return result;
+    }
+}

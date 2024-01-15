237e801866c37792fe0b4d810e8ceb4ea5f74669
DENISE.DL <denise.dl@hotmail.it>
2024-01-15 10:29:07 +0100

    fatto

---
+++ Esercizio32_OperatoreTernario/src/Main.java
@@ -0,0 +1,53 @@
+//Copia il seguente programma Java e sostutuisci il costrutto if-else con l'operatore ternario
+//
+// public class Main {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 100; i++) {
+//             System.out.println("Valore: " + i);
+//             System.out.println(checkValue(i));
+//         }
+//     }
+//
+//     private static String checkValue(int value) {
+//         String result = "";
+//
+//         if (value % 5 == 0) {
+//             result = "FizzBuzz";
+//         } else  {
+//             result = "Buzz";
+//         }
+//         return result;
+//     }
+//
+// }
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 100; i++) {
+            System.out.println("Valore: " + i);
+             System.out.println(checkValue(i));
+             checkValue(10);
+        }
+     }
+
+    private static String checkValue(int value) {
+        String result = "";
+
+        /* if (value % 5 == 0) {
+            result = "FizzBuzz";
+         } else  {
+         result = "Buzz";
+        }
+        return result;
+     }*/
+
+     final String checkValueStr = value % 5 == 0 ? "Fizz Buzz" : "Buzz";
+     return checkValueStr;
+
+
+    }
+}
